336559a740c58290bd0ee9cfcfaa6e99b4383d70
Lucas-B-Iori <lucasbertolassiiori@gmail.com>
2024-05-24 13:37:35 -0300

    n sei

--- src/components/IAComponents/TelaIa/TelaIa.jsx
+++ src/components/IAComponents/TelaIa/TelaIa.jsx
@@ -4,7 +4,8 @@ import { IoMdClose  } from "react-icons/io";
 import { IoSend } from "react-icons/io5";
 import { useState } from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+// import { run } from '@/data/gemini';
 
 const dropIn = {
   hidden: {
@@ -33,26 +34,83 @@ const dropIn = {
   },
 };
 
+const API_KEY = 'AIzaSyDyi_l85WTZcYGG7uYvnwatvKfYiNNmN6Q'
+const genAI = new GoogleGenerativeAI(API_KEY)
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"})
+
+// const genAI = new GoogleGenerativeAI('AIzaSyDyi_l85WTZcYGG7uYvnwatvKfYiNNmN6Q');
 
 export default function TelaIa({ fechaTela, openTela }) {
-  const genAI = new GoogleGenerativeAI('AIzaSyDyi_l85WTZcYGG7uYvnwatvKfYiNNmN6Q');
 
   const [ messageValue, setMessageValue ] = useState('')
   const [ userMessage, setUserMessage ] = useState('Olá, digite alguma coisa para que eu possa te responder')
   const [ chatMessage, setChatMessage ] = useState('')
   
+  // async function run(e) {
+  //   // For text-only input, use the gemini-pro model
+  //   e.preventDefault()
+  //   setUserMessage(messageValue)
+  //   setMessageValue('')
+  //   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  //   const result = await model.generateContent(messageValue);
+  //   const response = await result.response;
+  //   const text = response.text();
+  //   setChatMessage(text)
+  // }
+
   async function run(e) {
-    // For text-only input, use the gemini-pro model
     e.preventDefault()
+    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
     setUserMessage(messageValue)
     setMessageValue('')
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
     const result = await model.generateContent(messageValue);
     const response = await result.response;
     const text = response.text();
-    setChatMessage(text)
+    console.log(text);
   }
+//gemini-1.5-flash-latest
+  async function run(e) {
+    // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
+    e.preventDefault()
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
+  
+    const chat = model.startChat({
+      history: [
+        {
+          role: "user",
+          parts: [{ text: "Ola, este site é um protótipo de um novo site do Hospital das Clinicas de São Paulo" }],
+        },
+        // {
+        //   role: "model",
+        //   parts: [{ text: "Ótimo, o que você gostaria de saber" }],
+        // },
+      ],
+      generationConfig: {
+        candidateCount: 1,
+        temperature: 0.3,
+      },
+    });
+    setUserMessage(messageValue)
+    // const msg = "How many paws are in my house?";
+    
+    const result = await chat.sendMessage(messageValue);
+    setMessageValue('')
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+  // async function conversaIa(e) {
+  //   e.preventDefault()
+  //   setUserMessage(messageValue)
+  //   setMessageValue('')
+  //   let response = await run(messageValue)
+  //   console.log(response)
+  //   // let text = await run(messageValue)
+  //   // setChatMessage(text)
+  // }
   
   return (
     <motion.aside 
